89b0d2b695a6986c9f5ab7e8b4a48f0f28b86801
Alapan Ghosh <56732328+AlapanGhosh10@users.noreply.github.com>
2022-12-07 22:55:34 +0530

    LC 49 added

---
+++ 49_Group_Anagrams.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> res;
+        unordered_map<string, int> map;
+        int count = 0;
+        string str;
+        for(int i = 0 ; i < strs.size() ; i++) {
+            str = strs[i];
+            sort(strs[i].begin(), strs[i].end());
+            if(map.find(strs[i]) == map.end()) {
+                map[strs[i]] = count;
+                vector<string> now;
+                now.push_back(str);
+                res.push_back(now);
+                count++;
+            } else {
+                int index = map.find(strs[i])->second;
+                res[index].push_back(str);
+            }
+        }
+        return res;
+    }
+};
+
+*************
+ Explanation
+*************
+/* Create a unordered_map<sorted string, index in res>
+count variable to keep track of the last index inserted in res
+Only one for loop to traverse all elements {
+    store the string in a variable, sort the string in array itself
+    If (element not present in map) {
+        add it to the map with second value as count
+        create a vector push the stored string then push into res
+        increment count
+    } else {
+        find the index by using find() as the key is present
+        -> second to get the value, as find() returns iterator
+        add the original string to res in the index we found
+    }
+return res */
